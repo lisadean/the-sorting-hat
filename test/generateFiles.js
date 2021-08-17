5363da23c2caf806e75e1b44b6901db3719733f2
Lisa Dean <lisa.dean@supply.com>
2021-08-17 17:46:55 -0400

    updated generateFiles

--- test/generateFiles.js
+++ test/generateFiles.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const child = require('child_process');
 
 const lineCount = process.argv[2];
 
@@ -8,5 +9,10 @@ for (let i = 0; i < lineCount; i++) {
 	stuff += `${i + 1}${i < lineCount - 1 ? `\n` : ''}`;
 }
 const fileName = path.join(__dirname, 'fixtures', `${lineCount}lines.txt`);
+console.log(fileName);
 
-fs.writeFileSync(fileName, stuff);
+fs.writeFile(fileName, stuff, null, () =>
+	child.exec(`git add ${fileName}; git commit -m '${lineCount}'; git push`, (error, stdout, stderr) =>
+		console.log(`error: ${error} stdout: ${stdout} stderr: ${stderr}`)
+	)
+);
